178bf318a3eefe72c7f67053c4a63e431d7b4ff2
Nicole Zolnier <nzolnier@gmail.com>
2023-09-01 16:30:50 -0400

    praying

--- src/app/components/Join.tsx
+++ src/app/components/Join.tsx
@@ -8,7 +8,7 @@ interface Prop {
 
 const Join = ({isMobile} : Prop) => {
   return (<div className={styles.split}>
-    <section className="rounded-md 2xl:w-[100rem] 2xl:h-[25rem] lg:w-[71rem] w-[90%] lg:h-[22rem] h-[18rem] absolute z-10 bg-[#2A3342] text-white flex justify-around items-center">
+    <section className="rounded-md lg:w-[80%] w-[90%] lg:h-[22rem] h-[18rem] absolute z-10 bg-[#2A3342] text-white flex justify-around items-center">
       <article className="2xl:w-[30rem] lg:w-[26rem] w-[12rem]">
         <h3 className="2xl:text-6xl lg:text-4xl text-2xl font-bold pb-4">Join our Familia!</h3>
         <p className="2xl:text-2xl text-slate-400 lg:text-xl text-sm font-medium" >You don&apos;t need to be hispanic to be part of this change</p>
