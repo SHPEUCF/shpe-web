487220377201018d79cee69ee025dcc825ea3629
Nicole Zolnier <nzolnier@gmail.com>
2023-09-13 23:51:15 -0400

    fix logos

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,9 @@
 import logo from "../assets/logo-footer.svg"
 
 const Footer = () => {
-    return (<div className="bg-[#2A3342] text-white 2xl:h-[35rem] h-[20rem] pt-3 flex flex-col justify-around items-center text-slate-400">
+    return (<div className="bg-[#2A3342] text-white 2xl:h-[35rem] h-[25rem] pt-3 flex flex-col justify-around items-center text-slate-400">
       <img className="2xl:w-[10%]" src={logo.src}/>
-      <ul className="2xl:text-2xl flex justify-around lg:w-[50%] w-[100%] text-slate-400">
+      <ul className="2xl:text-2xl flex justify-around lg:w-[50%] w-[100%] text-slate-400 text-center align-center">
         <li><a href="#about">About</a></li>
         <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSd-afMh4hdQtk6HfmeaEOxptbqrMK4Nei9ukQXQBaFB0rwKOQ/viewform" target="_blank">News</a></li>
         <li><a href="#team">Our Team</a></li>
