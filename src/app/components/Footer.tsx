fe06679a0d972c5e8685ca823eafc36413dc8954
Nicole Zolnier <nzolnier@gmail.com>
2023-09-20 12:24:47 -0400

    fix pro

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import logo from "../assets/logo-footer.svg"
 
 const Footer = () => {
-    return (<div className="bg-[#2A3342] text-white 2xl:h-[35rem] h-[25rem] pt-3 flex flex-col justify-around items-center text-slate-400">
+    return (<div className="bg-[#2A3342] text-white 2xl:h-[35rem] h-[30rem] pt-3 flex flex-col justify-around items-center text-slate-400">
       <img className="2xl:w-[6%]" src={logo.src}/>
       <ul className="2xl:text-2xl flex justify-around lg:w-[50%] w-[100%] text-slate-400 text-center align-center">
         <li><a href="#about">About</a></li>
